769057ba6bb28a17c7344411b1493186dbe98a34
anuj97 <anujupadhyayjnp@gmail.com>
2020-07-16 17:02:18 +0530

    Heroku Deployment #1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+var path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
